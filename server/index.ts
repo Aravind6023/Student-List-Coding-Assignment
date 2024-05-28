6cbd562ef892bf38da8a384655a90eda1792fcbc
Aravind6023 <c23aravind@gmail.com>
2024-05-29 01:37:08 +0530

    vercel config

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,13 @@ export const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(cors());
+
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific HTTP methods
+    allowedHeaders: 'Content-Type,Authorization', // Allow specific headers
+    credentials: true
+}));
 
 /**
  * Start the Express server on the specified port.
